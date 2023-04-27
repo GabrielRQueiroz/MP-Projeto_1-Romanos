6e25f29b08613a8989a9d31b21734da0af429cd3
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2023-04-26 21:39:00 -0300

    🎉 init: Inicializa o projeto

---
+++ romanos.cpp
@@ -0,0 +1,8 @@
+/* Copyright 2023 Gabriel Roberto - O robertonto.
+===========================================*/
+
+#include "./romanos.hpp"
+
+int romanos_para_decimal(char const *num_romano) {
+  return 0;
+}
---
+++ testa_romanos.cpp
@@ -0,0 +1,19 @@
+/* Copyright 2023 Gabriel Roberto - O robertonto.
+===========================================*/
+
+
+#define CATCH_CONFIG_MAIN
+#include "./catch.hpp"
+#include "./romanos.hpp"
+
+TEST_CASE("Numeros romanos - algarismos únicos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("I") == 1);
+
+    REQUIRE(romanos_para_decimal("V") == 5);
+}
+
+TEST_CASE("Numeros romanos - algarismos inválidos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("G") == -1);
+
+    REQUIRE(romanos_para_decimal("i") == -1);
+}
