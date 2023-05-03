00e8b1274debfda761bab52dd4957ddd4caa8cff
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-05-03 20:22:52 -0300

    🧪 test: Combinações simples

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -21,3 +21,20 @@ TEST_CASE("Numeros romanos - algarismos inválidos", "[romanos]") {
 
     REQUIRE(romanos_para_decimal("i") == -1);
 }
+
+TEST_CASE("Numeros romanos - combinações simples", "[romanos]") {
+    REQUIRE(romanos_para_decimal("II") == 2);
+    REQUIRE(romanos_para_decimal("III") == 3);
+    REQUIRE(romanos_para_decimal("V") == 5);
+    REQUIRE(romanos_para_decimal("X") == 10);
+    REQUIRE(romanos_para_decimal("XX") == 20);
+    REQUIRE(romanos_para_decimal("XXX") == 30);
+    REQUIRE(romanos_para_decimal("L") == 50);
+    REQUIRE(romanos_para_decimal("C") == 100);
+    REQUIRE(romanos_para_decimal("CC") == 200);
+    REQUIRE(romanos_para_decimal("CCC") == 300);
+    REQUIRE(romanos_para_decimal("D") == 500);
+    REQUIRE(romanos_para_decimal("M") == 1000);
+    REQUIRE(romanos_para_decimal("MM") == 2000);
+    REQUIRE(romanos_para_decimal("MMM") == 3000);
+}
