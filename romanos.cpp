a490aae102b5615867b01022161b67b0c7008984
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2023-05-03 20:57:25 -0300

    ✨ feat: Combinações grandes
    Passa no teste de combinações mais complexas

--- romanos.cpp
+++ romanos.cpp
@@ -24,9 +24,38 @@ int roman_value(char num_romano) {
   return -1;
 }
 
+bool exceeded_consecutive(char num_romano, int consecutive) {
+  if (num_romano == 'I' || num_romano == 'X' || num_romano == 'C') {
+    return consecutive > 3;
+  }
+
+  if (num_romano == 'V' || num_romano == 'L' || num_romano == 'D') {
+    return consecutive > 1;
+  }
+
+  return false;
+}
+
+bool valid_successor(char num_romano, char successor) {
+  if (num_romano == 'I') {
+    return successor == 'V' || successor == 'X';
+  }
+
+  if (num_romano == 'X') {
+    return successor == 'L' || successor == 'C';
+  }
+
+  if (num_romano == 'C') {
+    return successor == 'D' || successor == 'M';
+  }
+
+  return false;
+}
+
 int romanos_para_decimal(const char* num_romano) {
   int decimal = 0;
   int input_size = 0;
+  int consecutive = 1;
 
   while (num_romano[input_size] != '\0') {
     input_size++;
@@ -35,8 +64,22 @@ int romanos_para_decimal(const char* num_romano) {
   for (int i = 0; i < input_size; i++) {
     int current_value = roman_value(num_romano[i]);
 
-    
-    decimal += current_value;
+    if (i > 0 && num_romano[i] == num_romano[i - 1]) {
+      consecutive++;
+    } else {
+      consecutive = 1;
+    }
+
+    if (i > 0 && current_value > roman_value(num_romano[i - 1])) {
+      decimal +=
+        current_value - roman_value(num_romano[i - 1]);
+    } else if (current_value < roman_value(num_romano[i + 1])) {
+      // Se o caractere atual for menor que o próximo, ele será subtraído
+      // na subtraído próxima iteração
+      continue;
+    } else {
+      decimal += current_value;
+    }
   }
 
   return decimal;
